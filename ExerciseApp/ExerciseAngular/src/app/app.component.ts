ab95bbafcbd93b697c7714063d38802daf9fb0f1
Eric Adams <64985325+eadams98@users.noreply.github.com>
2022-01-10 11:39:38 -0600

    unsaved

--- ExerciseApp/ExerciseAngular/src/app/app.component.ts
+++ ExerciseApp/ExerciseAngular/src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,43 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ExerciseApp';
+  validUser: boolean = false;
+  
+  loginForm!: FormGroup; 
+  
+  /*new FormGroup({
+    userName: new FormControl(''),
+    password: new FormControl(''),
+  })*/
+
+  /*profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });*/
+
+  constructor(private fb: FormBuilder) {
+    /*this.loginForm = this.fb.group({
+      userName: [''],
+      password: ['']
+    })*/
+  }
+
+  ngOnInit() {
+    //this.createForm()
+  }
+
+  createForm() {
+    //this.loginForm = this.fb.group({
+      //userName: [''],
+      //password: ['']
+    //})
+  }
+
+  login() {
+    //if (this.loginForm.value.userName == "Eric" && this.loginForm.value.password == "root")
+    //  this.validUser = true; // reroute to next page
+    //else
+    //  this.validUser = false; // set to false and display proper msg
+  }
+
 }
--- ExerciseApp/ExerciseAngular/src/app/app.module.ts
+++ ExerciseApp/ExerciseAngular/src/app/app.module.ts
@@ -4,13 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+// New Imports
+import { FormBuilder, FormGroup, Validators, FormsModule, ReactiveFormsModule } from '@angular/forms'
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    BrowserModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
